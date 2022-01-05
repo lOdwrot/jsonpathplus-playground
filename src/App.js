919c508a04c41cab55c5db7274763d4943e9be6b
lodwrot <extern.lukasz.odwrot@vwfs.io>
2022-01-05 15:38:42 +0100

    implemented JSONPATH playground

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { JSONPath } from 'jsonpath-plus';
+import { useEffect, useState } from 'react';
+import JSONPretty from 'react-json-pretty';
+
+const samplePattern =
+    '$..[?(@["@Domain"]=="VW.NEW" && (@.Vehicle.Key.match(/^(E11|E21)/)))]';
+const sampleInput = `{
+  "Request": {
+    "test": 15,
+    "@Domain": "VW.NEW",
+    "@Name": "Products",
+    "Vehicle": {
+      "ID": "E11XPD",
+      "Key": "E11XPD",
+      "Year": "2022",
+      "PriceTotal": "122",
+      "PriceModel": "122",
+      "PriceDetails": {
+        "VatMode": "Gross"
+      },
+      "SalesgroupKey": "111",
+      "CarlineKey": "111",
+      "Version": "222"
+    }
+  }
+}`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [inputJson, setInputJson] = useState(sampleInput);
+    const [pattern, setPattern] = useState(samplePattern);
+    const [pathResult, setPathResult] = useState('');
+
+    useEffect(() => {
+        let resultJson;
+        try {
+            resultJson = JSON.parse(inputJson);
+        } catch (e) {
+            setPathResult('Invalid JSON input');
+            return;
+        }
+
+        try {
+            setPathResult(JSONPath(pattern, resultJson));
+        } catch (err) {
+            setPathResult(
+                'Invalid JSONPATH pattern or can not execute on current structure'
+            );
+        }
+    }, [pattern, inputJson]);
+
+    const reinit = () => {
+        setInputJson(sampleInput);
+        setPattern(samplePattern);
+    };
+    return (
+        <div style={{ padding: '2px' }}>
+            <div>
+                <button onClick={reinit}>Reset</button>
+            </div>
+            <input
+                style={{ width: '99vw' }}
+                value={pattern}
+                onChange={(e) => setPattern(e.target.value)}
+            />
+            <br />
+            <div style={{ display: 'flex' }}>
+                <div style={{ width: '620px' }}>
+                    <textarea
+                        onChange={(e) => setInputJson(e.target.value)}
+                        style={{ maxWidth: '610px' }}
+                        value={inputJson}
+                        name="w3review"
+                        rows="50"
+                        cols="80"
+                    />
+                </div>
+                <div
+                    style={{
+                        minWidth: '300px',
+                        height: '800px',
+                        overflowY: 'auto',
+                    }}
+                >
+                    <JSONPretty data={pathResult}></JSONPretty>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
